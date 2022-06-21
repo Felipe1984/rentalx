614e94b7b70a6f711f5362d6965b989d20a660e0
Felipe dos Anjos <felipe.b.abencoado@gmail.com>
2022-06-20 21:26:53 -0300

    chore: add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node
+
+LABEL author="Felipe dos Anjos"
+
+WORKDIR /usr/app
+COPY package.json ./
+RUN npm install
+COPY . .
+EXPOSE 3333
+
+CMD ["npm", "run", "dev"]
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/controllers/createCategory";
+import { importCategoryController } from "../modules/cars/controllers/importCategory";
 import { listCategoriesController } from "../modules/cars/controllers/listCategories";
 
 const categoriesRoutes = Router();
-// const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,6 +17,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
